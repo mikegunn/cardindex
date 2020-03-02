366794889d8516cf5fe8b73fdac7c3f318466889
Mike Gunn <miikegunn@gmail.com>
2020-03-02 23:31:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-    $.getJSON("https://spreadsheets.google.com/feeds/list/17gxSsNCJ4nYTV3npmlWgjJL6l_461mqEgY8d0ct4q7s/od6/public/values?alt=json", function (data) {
+    $.getJSON("https://spreadsheets.google.com/feeds/list/1t2_HHLkibAybPORmXuDDEFEyetC3p7r1blQRpjzinXg/od6/public/values?alt=json", function (data) {
 
       var sheetData = data.feed.entry;
 
       var i;
       for (i = 0; i < sheetData.length; i++) {
 
-        var name = data.feed.entry[i]['gsx$position']['$t'];
-        var age = data.feed.entry[i]['gsx$games']['$t'];
-        var email = data.feed.entry[i]['gsx$referee']['$t'];
+        var name = data.feed.entry[i]['gsx$_cn6ca']['$t'];
+        var age = data.feed.entry[i]['gsx$_cokwr']['$t'];
+        var email = data.feed.entry[i]['gsx$_cpzh4']['$t'];
 
         document.getElementById('demo').innerHTML += ('<tr>'+'<td>'+name+'</td>'+'<td>'+age+'</td>'+'<td>'+email+'</td>'+'</tr>');
 
